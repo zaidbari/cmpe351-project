d63a7e76972d5489f5b5cd061bb2d8d2396a70c5
= <=>
2022-12-07 22:15:37 +0200

    Input command line arguments

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,38 @@
+/* -------------------------------- Libraries ------------------------------- */
 #include <iostream>
+#include <unistd.h>
+#include <string.h>
 
-int main()
+/* ------------------------------- Namespaces ------------------------------- */
+using namespace std;
+
+/**
+ * @brief Main entry point
+ * @param argc Argument count (contains the number of arguments)
+ * @param argv  Argument vector (contains argument's values)
+ *
+ * @return int EXIT_SUCCESS = 1
+ */
+int main(int argc, char *argv[])
 {
 
-	std::cout << "Hello, World!" << std::endl;
+	char *input_file_name, *output_file_name;
+	for (int i = 1; i < argc; i++)
+	{
+		if (i + 1 != argc)
+		{
+			if (strcmp(argv[i], "-f") == 0)
+			{
+				input_file_name = argv[i + 1];
+				i++;
+			}
+			if (strcmp(argv[i], "-o") == 0)
+			{
+				output_file_name = argv[i + 1];
+				i++;
+			}
+		}
+	}
 
-	return 0;
+	exit(EXIT_SUCCESS);
 }
