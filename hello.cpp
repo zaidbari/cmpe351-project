e0b3d286116691dbfe790b502024519db733a851
= <=>
2022-12-10 17:57:59 +0200

    input structorization

--- hello.cpp
+++ hello.cpp
@@ -9,18 +9,33 @@
 /* ------------------------------- Namespaces ------------------------------- */
 // using namespace std;
 
-/* -------------------------- variable definitaions ------------------------- */
+/* -------------------------- Variable definitaions ------------------------- */
 #define DELIMETER ':'
 
-auto getCommandLineArguments(int argc, char *argv[])
+/* ------------------------- Structure definiations ------------------------- */
+struct schedule_table
+{
+	int burst_time;
+	int arrival_time;
+	int priority;
+};
+
+struct filenames
+{
+	char *output_file;
+	char *input_file;
+};
+
+/**
+ * @brief get command line arguments
+ * @param argc
+ * @param argv
+ * @return filenames structure
+ */
+filenames getCommandLineArguments(int argc, char *argv[])
 {
 
 	char *input_file_name, *output_file_name;
-	struct filenames
-	{
-		char *output_file;
-		char *input_file;
-	};
 
 	for (int i = 1; i < argc; i++)
 		if (i + 1 != argc)
@@ -36,6 +51,48 @@ auto getCommandLineArguments(int argc, char *argv[])
 }
 
 /**
+*	@brief
+*	@param input_file_name
+
+*	@return std::vector<schedule_table>
+*/
+std::vector<schedule_table> readInputFile(std::string input_file_name)
+{
+	/* ------------------ input/output file stream declarations ----------------- */
+	std::ifstream inFile(input_file_name);
+
+	/* ---------------------- vector to store input values ---------------------- */
+	std::vector<schedule_table> values;
+
+	if (inFile.is_open())
+	{
+		std::string line;
+		while (std::getline(inFile, line))
+		{
+			schedule_table row;
+
+			std::stringstream ss(line);
+			std::string burst_time, arrival_time, priority;
+
+			std::getline(ss, burst_time, DELIMETER);
+			std::getline(ss, arrival_time, DELIMETER);
+			std::getline(ss, priority, DELIMETER);
+
+			/* ------------------ row of input values from single line ------------------ */
+			row.burst_time = stoi(burst_time);
+			row.arrival_time = stoi(arrival_time);
+			row.priority = stoi(priority);
+
+			values.push_back(row);
+		}
+
+		inFile.close();
+	}
+
+	return values;
+}
+
+/**
  * @brief Main entry point
  * @param argc Argument count (contains the number of arguments)
  * @param argv  Argument vector (contains argument's values)
@@ -47,44 +104,15 @@ int main(int argc, char *argv[])
 
 	/* -------------- get command line arguments and store results -------------- */
 	auto [input_file_name, output_file_name] = getCommandLineArguments(argc, argv);
+	std::vector<schedule_table> input_values = readInputFile(input_file_name);
 
-	/* ------------------ input/output file stream declarations ----------------- */
-	std::ifstream inFile(input_file_name);
-
-	/* ---------------------- vector to store input values ---------------------- */
-	std::vector<std::vector<int>> values;
-
-	if (inFile.is_open())
+	for (int i = 0; i < input_values.size(); i++)
 	{
-		std::string line;
-		while (std::getline(inFile, line))
-		{
-
-			std::vector<int> row;
-
-			std::stringstream ss(line);
-			std::string burst_time, arrival_time, priority;
-
-			std::getline(ss, burst_time, DELIMETER);
-			std::getline(ss, arrival_time, DELIMETER);
-			std::getline(ss, priority, DELIMETER);
-
-			/* ------------------ row of input values from single line ------------------ */
-			row.push_back(stoi(burst_time));
-			row.push_back(stoi(arrival_time));
-			row.push_back(stoi(priority));
-
-			values.push_back(row);
-		}
-
-		inFile.close();
-	}
-
-	for (int i = 0; i < values.size(); i++)
-	{
-		for (int j = 0; j < values[i].size(); j++)
-			std::cout << values[i][j] << " ";
-		std::cout << std::endl;
+		std::cout
+				<< " " << input_values[i].burst_time
+				<< " " << input_values[i].arrival_time
+				<< " " << input_values[i].priority
+				<< std::endl;
 	}
 
 	exit(EXIT_SUCCESS);
